902a382129540832165eab3dd2f7d5444e30b207
Diyatayal <128578106+Diyatayal@users.noreply.github.com>
2025-05-17 11:17:23 +0000

    commited

--- App.py
+++ App.py
@@ -12,23 +12,26 @@ import pandas as pd
 import base64, random
 import time, datetime
 from pyresparser import ResumeParser
-from pdfminer3.layout import LAParams, LTTextBox
-from pdfminer3.pdfpage import PDFPage
-from pdfminer3.pdfinterp import PDFResourceManager
-from pdfminer3.pdfinterp import PDFPageInterpreter
-from pdfminer3.converter import TextConverter
+from pdfminer.layout import LAParams, LTTextBox
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+
 import io, random
 from streamlit_tags import st_tags
 from PIL import Image
 import sqlite3
 from Courses import ds_course, web_course, android_course, ios_course, uiux_course, resume_videos, interview_videos
-import pafy
+from yt_dlp import YoutubeDL
 import plotly.express as px
-import youtube_dl
+
 
 def fetch_yt_video(link):
-    video = pafy.new(link)
-    return video.title
+    ydl_opts = {}
+    with YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(link, download=False)
+        return info.get("title", "No Title Found")
 
 
 def get_table_download_link(df, filename, text):
